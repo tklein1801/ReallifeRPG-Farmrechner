ddab0ee66bddeeb570eaddcd85e4f258625f052c
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-02-10 13:33:00 +0100

    Get changelogs from changelogs.json & run Prettier

--- src/components/Changelog.js
+++ src/components/Changelog.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from '../style/Stylesheet';
+import React from "react";
+import { changelogs } from "../data/changelogs.json";
+import { Text, View } from "react-native";
+import styles from "../style/Stylesheet";
 
 const Changelog = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.headerTextStyle}>Changelog</Text>
-
-            <View style={styles.changelogCard}>
-                <Text style={styles.changelogHead}>Beta 0.2V</Text>
-                <Text>- Illegale Farmrouten werden nun mit dem richtigen Multipikator angezeigt</Text>
-                <Text>- Design etwas angepasst</Text>
-                <Text>- Changelog eingefügt</Text>
-            </View>
-        </View>
-
-    )
-}
+  const reversedChangelog = changelogs.reverse(); // Now they should be sort by release date
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerTextStyle}>Changelog</Text>
+      {reversedChangelog.map((changelog, index) => {
+        return (
+          <View kex={index} style={styles.changelogCard}>
+            <Text style={styles.changelogHead}>{changelog.version}</Text>
+            {changelog.changes.map((change, changeIndex) => {
+              return <Text key={changeIndex}>{change}</Text>;
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
+};
 
 export default Changelog;
